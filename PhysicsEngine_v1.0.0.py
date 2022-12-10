77a318d7d1954bdcd6930c8a452541648d1d883f
Todd Lock <tolock5@gmail.com>
2022-12-10 13:21:58 -0500

    Added different sized circles with weighted momentum

--- PhysicsEngine_v1.0.0.py
+++ PhysicsEngine_v1.0.0.py
@@ -16,7 +16,6 @@ NUMBER_BALLS = 100
 
 GRAVITY_ACCELERATION = -9.81
 GRAVITY_ACCELERATION_PER_TICK = GRAVITY_ACCELERATION / UPDATES_RATE_PER_SECOND
-BALL_MASS_KG = 500.0
 CEILING_SPRING_CONSTANT = 100.0
 GROUND_HEIGHT = 50.0
 CLICK_SPRING_CONSTANT = 500.0
@@ -25,6 +24,7 @@ ROLLING_FRICTION_COEFFICIENT_PER_SECOND = ROLLING_FRICTION_COEFFICIENT / UPDATES
 
 GRAVITY_ENABLED = True
 SPRING_ENABLED = False
+AIR_FRICTION_ENABLED = True
 
 SPRING_X = 150
 SPRING_Y = 300
@@ -36,7 +36,8 @@ MAX_Y = 800.0
 
 BALL_STARTS = [(random.randrange(int(MAX_X)), random.randrange(int(MAX_Y))) for i in range(NUMBER_BALLS)]
 BALLS = []
-BALL_RADIUS = 10
+BALL_MIN_RADIUS = 10
+BALL_MAX_RADIUS = 20
 BALL_COLORS = ["white", "red", "green", "blue", "yellow", "orange", "purple", "black", "cyan", "magenta"]
 
 # globals
@@ -80,8 +81,9 @@ def main():
 
     for ball_x, ball_y in BALL_STARTS:
         color = BALL_COLORS[random.randrange(len(BALL_COLORS))]
-        BALLS.append([canvas.create_oval(ball_x - BALL_RADIUS, MAX_Y - (ball_y - BALL_RADIUS),
-                                         ball_x + BALL_RADIUS, MAX_Y - (ball_y + BALL_RADIUS), fill=color), 0.0, 0.0])
+        radius = ((BALL_MAX_RADIUS - BALL_MIN_RADIUS) * random.random()) + BALL_MIN_RADIUS
+        BALLS.append([canvas.create_oval(ball_x - radius, MAX_Y - (ball_y - radius),
+                                         ball_x + radius, MAX_Y - (ball_y + radius), fill=color), 0.0, 0.0, radius])
 
     # add to window and show
     canvas.pack()
@@ -92,8 +94,9 @@ def main():
     frames = 0
 
     while run:
-        for index, (ball, x_velocity, y_velocity) in enumerate(BALLS):
+        for index, (ball, x_velocity, y_velocity, radius) in enumerate(BALLS):
             ball_x, ball_y = ball_center(canvas.coords(ball))
+            ball_mass = math.pi * radius * radius
 
             # print("Ball {0} center x {1} y {2}".format(index, ball_x, ball_y))
 
@@ -101,17 +104,18 @@ def main():
                 y_velocity += GRAVITY_ACCELERATION_PER_TICK
             if SPRING_ENABLED:
                 spring_distance = euclidean_distance(ball_x, ball_y, SPRING_X, SPRING_Y)
-                spring_acceleration = ((CEILING_SPRING_CONSTANT * spring_distance) / BALL_MASS_KG) / UPDATES_RATE_PER_SECOND
+                spring_acceleration = ((CEILING_SPRING_CONSTANT * spring_distance) / ball_mass) / UPDATES_RATE_PER_SECOND
                 spring_angle_radians = angle_horizon(ball_x, ball_y, SPRING_X, SPRING_Y)
                 spring_acceleration_x = spring_acceleration * math.cos(spring_angle_radians)
                 spring_acceleration_y = spring_acceleration * math.sin(spring_angle_radians)
 
                 x_velocity += spring_acceleration_x
                 y_velocity += spring_acceleration_y
+
             global Clicked, ClickLocationX, ClickLocationY
             if Clicked:
                 click_distance = euclidean_distance(ball_x, ball_y, ClickLocationX, ClickLocationY)
-                click_acceleration = ((CLICK_SPRING_CONSTANT * click_distance) / BALL_MASS_KG) / UPDATES_RATE_PER_SECOND
+                click_acceleration = ((CLICK_SPRING_CONSTANT * click_distance) / ball_mass) / UPDATES_RATE_PER_SECOND
                 click_angle_radians = angle_horizon(ClickLocationX, ClickLocationY, ball_x, ball_y)
                 click_acceleration_x = click_acceleration * math.cos(click_angle_radians)
                 click_acceleration_y = click_acceleration * math.sin(click_angle_radians)
@@ -120,10 +124,9 @@ def main():
                 y_velocity -= click_acceleration_y
 
             # Apply friction
-            #x_velocity -= pow(x_velocity, 2) * ROLLING_FRICTION_COEFFICIENT
-            #y_velocity -= pow(y_velocity, 2) * ROLLING_FRICTION_COEFFICIENT
-            x_velocity -= x_velocity * ROLLING_FRICTION_COEFFICIENT_PER_SECOND
-            y_velocity -= y_velocity * ROLLING_FRICTION_COEFFICIENT_PER_SECOND
+            if AIR_FRICTION_ENABLED:
+                x_velocity -= x_velocity * ROLLING_FRICTION_COEFFICIENT_PER_SECOND
+                y_velocity -= y_velocity * ROLLING_FRICTION_COEFFICIENT_PER_SECOND
 
             # check if ball will be outside boundaries
             # if crossing boundary, bounce by inverting and losing some velocity
@@ -131,11 +134,13 @@ def main():
             proposed_ball_x = ball_x + x_velocity
 
             # ball collision checking           O(N^2)!!!!!!
-            for other_index, (other_ball, other_x_velocity, other_y_velocity) in enumerate(BALLS):
+            for other_index, (other_ball, other_x_velocity, other_y_velocity, other_radius) in enumerate(BALLS):
                 if other_index > index:
                     other_ball_x, other_ball_y = ball_center(canvas.coords(other_ball))
-                    if abs(proposed_ball_x - other_ball_x) < (2.0 * BALL_RADIUS) or abs(proposed_ball_y - other_ball_y) < (2.0 * BALL_RADIUS):
-                        if euclidean_distance(proposed_ball_x, proposed_ball_y, other_ball_x, other_ball_y) < (2.0 * BALL_RADIUS):
+                    other_ball_mass = math.pi * other_radius * other_radius
+                    combined_radii = radius + other_radius
+                    if abs(proposed_ball_x - other_ball_x) < combined_radii or abs(proposed_ball_y - other_ball_y) < combined_radii:
+                        if euclidean_distance(proposed_ball_x, proposed_ball_y, other_ball_x, other_ball_y) < combined_radii:
                             # print("Collision! Balls {0} and {1} collided at X {2} Y {3}, other X {4} Y {5}".format(index, other_index, proposed_ball_x, proposed_ball_y, other_ball_x, other_ball_y))
 
                             tangent_vector_x = ball_y - other_ball_y
@@ -149,26 +154,25 @@ def main():
                             velocity_component_on_tangent = [component * dot_product for component in tangent_vector_normalized]
                             velocity_component_perpendicular_to_tangent = [i-j for (i, j) in zip(relative_velocity, velocity_component_on_tangent)]
 
-                            x_velocity -= velocity_component_perpendicular_to_tangent[0]
-                            y_velocity -= velocity_component_perpendicular_to_tangent[1]
+                            weight_ratio = ball_mass / other_ball_mass
+                            weight_ratio_inverted = 1.0 / weight_ratio
 
-                            BALLS[other_index][1] += velocity_component_perpendicular_to_tangent[0]
-                            BALLS[other_index][2] += velocity_component_perpendicular_to_tangent[1]
+                            x_velocity -= velocity_component_perpendicular_to_tangent[0] / weight_ratio
+                            y_velocity -= velocity_component_perpendicular_to_tangent[1] / weight_ratio
+
+                            BALLS[other_index][1] += velocity_component_perpendicular_to_tangent[0] / weight_ratio_inverted
+                            BALLS[other_index][2] += velocity_component_perpendicular_to_tangent[1] / weight_ratio_inverted
 
             proposed_ball_y = ball_y + y_velocity
             proposed_ball_x = ball_x + x_velocity
 
-            if ball_x > MIN_X + BALL_RADIUS >= proposed_ball_x:
-                # print("Ball {0} bounced x left! X {0} proposed {1}".format(index, ball_x, proposed_ball_x))
+            if ball_x > MIN_X + radius >= proposed_ball_x:
                 x_velocity *= -0.8
-            elif ball_x < MAX_X - BALL_RADIUS < proposed_ball_x:
-                # print("Ball {0} bounced x right! X {0} proposed {1}".format(index, ball_x, proposed_ball_x))
+            elif ball_x < MAX_X - radius < proposed_ball_x:
                 x_velocity *= -0.8
-            if ball_y > MIN_Y + BALL_RADIUS > proposed_ball_y:
-                # print("Ball {0} bounced y bottom! Y {0} proposed {1}".format(index, ball_y, proposed_ball_y))
+            if ball_y > MIN_Y + radius > proposed_ball_y:
                 y_velocity *= -0.8
-            elif ball_y < MAX_Y - BALL_RADIUS < proposed_ball_y:
-                # print("Ball {0} bounced y top! Y {0} proposed {1}".format(index, ball_y, proposed_ball_y))
+            elif ball_y < MAX_Y - radius < proposed_ball_y:
                 y_velocity *= -0.8
 
             move_ball(canvas, ball, x_velocity, y_velocity)
